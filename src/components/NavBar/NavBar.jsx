cb60de118b674fa4b708da1a756fc53318ce586b
Axel Langerman <solomonsilversmith@gmail.com>
2022-09-25 18:32:50 -0300

    Trabajando en la pre-entrega

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,10 @@
 import "./NavBar.css";
+import pngwing from "./assets/pngwing.com.png";
 
 export const NavBar = () => {
     return(
       <nav className="navegation">
-        <img className="logo" src={require("./assets/pngwing.com.png").default} alt="logo"/>
+        <img className="logo" src={pngwing} alt="logo"/>
         <ul className="list">
             <li><a href="/">Home</a></li>
             <li><a href="/">Productos</a></li>
